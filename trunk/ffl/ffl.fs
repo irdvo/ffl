3bb8e0b4069f9df36086c96bd4dc6e160bdfc020
dick <dick@4faefe48-5067-11de-9cb9-2f00d435f02c>
2005-12-14 19:27:44 +0000

    This commit was generated by cvs2svn to compensate for changes in r2, which included commits to RCS files with non-trunk default branches.

---
+++ trunk/ffl/ffl.fs
@@ -0,0 +1,35 @@
+\ ==============================================================================
+\
+\       ffl - the include-all source file in the forth foundation library
+\
+\               Copyright (C) 2005  Dick van Oudheusden
+\  
+\ This library is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public
+\ License as published by the Free Software Foundation; either
+\ version 2 of the License, or (at your option) any later version.
+\
+\ This library is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+\ General Public License for more details.
+\
+\ You should have received a copy of the GNU General Public
+\ License along with this library; if not, write to the Free
+\ Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+\
+\ ==============================================================================
+\ 
+\  $Date: 2005-12-14 19:27:43 $ $Revision: 1.1.1.1 $
+\
+\ ==============================================================================
+
+include ffl/chr.fs
+include ffl/crc.fs
+include ffl/tst.fs
+include ffl/scn.fs
+include ffl/scl.fs
+include ffl/sci.fs
+
+\ ==============================================================================
+
