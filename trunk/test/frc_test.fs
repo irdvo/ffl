dffba9bbad9371853a655d61dfd81e94316208cf
dick <dick@4faefe48-5067-11de-9cb9-2f00d435f02c>
2006-08-27 07:06:44 +0000

    frc cont.

--- trunk/test/frc_test.fs
+++ trunk/test/frc_test.fs
@@ -1,8 +1,8 @@
 \ ==============================================================================
 \
-\            ffl_test - the test-all source file in the ffl
+\         frc_test - the test words for the frc module in the ffl
 \
-\               Copyright (C) 2005  Dick van Oudheusden
+\               Copyright (C) 2006  Dick van Oudheusden
 \  
 \ This library is free software; you can redistribute it and/or
 \ modify it under the terms of the GNU General Public
@@ -20,33 +20,57 @@
 \
 \ ==============================================================================
 \ 
-\  $Date: 2006-08-27 07:06:44 $ $Revision: 1.13 $
+\  $Date: 2006-08-27 07:06:44 $ $Revision: 1.1 $
 \
 \ ==============================================================================
 
-
+include ffl/frc.fs
 include ffl/tst.fs
 
-tst-reset-tests
-
-\ the test sources
-include bar_test.fs
-include car_test.fs
-include chr_test.fs
-include chs_test.fs
-include crc_test.fs
-include dtm_test.fs
-include frc_test.fs
-include hct_test.fs
-include md5_test.fs
-include scl_test.fs
-include str_test.fs
-include tis_test.fs
-include tos_test.fs
-
-.( Forth Foundation Library Test: ) tst-get-result .  .( errors in ) . .( tests) cr
+.( Testing: frc) cr 
   
-bye
+\ Module words
+
+t{ 10  2 frc+calc-gcd 2 ?s }t
+t{ 2 -10 frc+calc-gcd 2 ?s }t
+
+t{ 5  15 frc+calc-lcm 15 ?s }t
+t{ 10 15 frc+calc-lcm 30 ?s }t
+
+\ Creation / allocation
+
+frc-create frc1
+
+t{ frc-new value frc2 }t
+
+t{ frc1 frc-get ?0 1 ?s  }t
+
+t{ 10 5 frc1 frc-set     }t
+
+t{ frc1 frc-get 1 ?s 2 ?s }t    \ normalized 5/10 -> 1/2
+
+\ Members
+
+t{ frc2 frc-get ?0 1 ?s  }t
+
+t{ 8 12 frc2 frc-set }t
+
+t{ frc2 frc-num@ 3 ?s   }t
+t{ frc2 frc-denom@ 2 ?s }t     \ normalized 12/8 -> 3/2
+
+\ Calculation
+
+t{ 6 4 frc1 frc-set }t
+t{ 5 2 frc2 frc-set }t
+
+\ t{ frc2 frc1 frc^add }t
+
+\ t{ frc1 frc-get 16 ?s 15 ?s }t
+
+\ t{ 5 3 frc1 frc-add }t
+
+\ t{ frc1 frc-get 5 ?s 3 ?s }t
+
+t{ frc2 frc-free }t
 
 \ ==============================================================================
-
