25a3432e205cc386914c71cbaf87814fe56f4312
dick <dick@4faefe48-5067-11de-9cb9-2f00d435f02c>
2006-09-17 19:03:05 +0000

    new module rng

--- test/rng_test.fs
+++ test/rng_test.fs
@@ -1,8 +1,8 @@
 \ ==============================================================================
 \
-\       ffl - the include-all source file in the forth foundation library
+\         rng_test - the test words for the rng module in the ffl
 \
-\               Copyright (C) 2005  Dick van Oudheusden
+\               Copyright (C) 2006  Dick van Oudheusden
 \  
 \ This library is free software; you can redistribute it and/or
 \ modify it under the terms of the GNU General Public
@@ -20,31 +20,51 @@
 \
 \ ==============================================================================
 \ 
-\  $Date: 2006-09-17 19:03:05 $ $Revision: 1.15 $
+\  $Date: 2006-09-17 19:03:05 $ $Revision: 1.1 $
 \
 \ ==============================================================================
 
-include ffl/bar.fs
-include ffl/car.fs
-include ffl/chr.fs
-include ffl/chs.fs
-include ffl/cpx.fs
-include ffl/crc.fs
-include ffl/dtm.fs
-include ffl/dti.fs
-include ffl/frc.fs
-include ffl/hcn.fs
-include ffl/hct.fs
-include ffl/hci.fs
-include ffl/md5.fs
-include ffl/tst.fs
 include ffl/rng.fs
-include ffl/scn.fs
-include ffl/scl.fs
-include ffl/sci.fs
-include ffl/str.fs
-include ffl/tis.fs
-include ffl/tos.fs
+include ffl/tst.fs
+
+
+[DEFINED] rng.version [IF]
+
+
+.( Testing: rng) cr 
+
+t{ 5489 rng-create rng1 }t
+
+t{ rng1 rng-next-number 3499211612 ?u }t
+
+: rng-skip  ( w:rng n - = skip n random numbers )
+  0 DO
+    dup rng-next-number drop
+  LOOP
+  drop
+;
+
+t{ rng1 622 rng-skip }t 
+
+t{ rng1 rng-next-number 4020325887 ?u }t
+t{ rng1 rng-next-number 4178893912 ?u }t
+t{ rng1 rng-next-number 610818241  ?u }t
+
+t{ rng1 9999 rng-skip }t
+
+t{ rng1 rng-next-number 862334504 ?u }t
+
+
+t{ 19650218 rng-new value rng2 }t
+
+t{ rng2 rng-next-float 0.54146918e0 ?r }t
+
+t{ rng2 9999 rng-skip }t
+
+t{ rng2 rng-next-float 0.95790732e0 ?r }t
+
+t{ rng2 rng-free }t
+
+[THEN]
 
 \ ==============================================================================
-
