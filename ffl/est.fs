3e21b4cc37fd051cadb4813f7e07cbe90b3416bc
dick <dick@4faefe48-5067-11de-9cb9-2f00d435f02c>
2007-12-02 07:50:43 +0000

    New module: string with escapted chars

---
+++ ffl/est.fs
@@ -0,0 +1,205 @@
+\ ==============================================================================
+\
+\                 est - the escaped string in the ffl
+\
+\               Copyright (C) 2007  Dick van Oudheusden
+\  
+\ This library is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public
+\ License as published by the Free Software Foundation; either
+\ version 2 of the License, or (at your option) any later version.
+\
+\ This library is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+\ General Public License for more details.
+\
+\ You should have received a copy of the GNU General Public
+\ License along with this library; if not, write to the Free
+\ Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+\
+\ ==============================================================================
+\ 
+\  $Date: 2007-12-02 07:50:42 $ $Revision: 1.1 $
+\
+\ ==============================================================================
+
+
+include ffl/config.fs
+
+
+[UNDEFINED] stt.version [IF]
+
+( estt= String with escaped characters )
+( The est module implements a string with escaped characters. The code is    )
+( inspired by the proposal for escaped string by Stephen Pelc and Peter      )
+( Knagss. The following conversion characters are translated:                )
+( <pre>                                                                      )
+( \a  - Bel = ascii 7                                                        )
+( \b  - Backspace = ascii 8                                                  )
+( \e  - Escape = ascii 27                                                    )
+( \f  - Formfeed = ascii 12                                                  )
+( \l  - Linefeed = ascii 10                                                  )
+( \m  - cr/lf = ascii 13,10                                                  )
+( \n  - new line                                                             )
+( \q  - Quote = ascii 34                                                     )
+( \r  - cr = ascii 13                                                        )
+( \t  - ht = ascii 9                                                         )
+( \v  - vt = ascii 11                                                        )
+( \z  - nul = ascii 0                                                        )
+( \"  - quote = ascii 34                                                     )
+( xhh - hex digit                                                            )
+( \\  - backslash                                                            )
+( </pre>                                                                     )
+
+
+1 constant est.version
+
+
+( Private words )
+
+create est-table
+       7 c,     \ a
+       8 c,     \ b
+  char c c,     \ c
+  char d c,     \ d
+      27 c,     \ e
+      12 c,     \ f
+  char g c,     \ g
+  char h c,     \ h
+  char i c,     \ i
+  char j c,     \ j
+  char k c,     \ k
+      10 c,     \ l
+  char m c,     \ m
+      10 c,     \ n
+  char o c,     \ o
+  char p c,     \ p
+  char " c,     \ q
+      13 c,     \ r
+  char s c,     \ s
+       9 c,     \ t
+  char u c,     \ u
+      11 c,     \ v
+  char w c,     \ w
+  char x c,     \ x
+  char y c,     \ y
+       0 c,     \ z
+  
+       
+: est-add-char   ( ch c-addr - = Add the character to the counted string )
+  tuck count + c!
+  dup c@ 1+ swap c!
+;
+
+
+: est-add-2hex   ( c-addr u c-addr - c-addr' u' = Add a character in two hex digits to the counted string )
+  base @ >r                  \ Save current base
+  >r
+  hex
+  0 0 2over 2 min            \ Maximum convert 2 hex digits
+  >r 
+  r@ >number
+  nip nip                    \ No interest in start of string and most significant cell
+  r> swap -                  \ Get converted length
+  swap r> est-add-char       \ Add result in string
+  /string                    \ Update source string
+  r> base !
+;
+
+
+[UNDEFINED] place [IF]
+: place   ( c-addr1 u1 c-addr2 - = Place string1 at address c-addr2 as counted string )
+  2dup c!
+  char+ swap cmove
+;
+[THEN]
+
+
+( String words )
+
+[UNDEFINED] parse-esc [IF]
+: parse-esc   ( c-addr u c-addr - c-addr' u' = Parse a escaped character )
+  over 0<> IF
+    >r
+    over c@
+    dup [char] x = IF
+      drop 
+      1 /string r@ est-add-2hex
+    ELSE
+      dup [char] m = IF
+        drop 
+        13 r@ est-add-char
+        10 r@ est-add-char
+      ELSE
+        dup [char] n = IF
+          drop
+          end-of-line count tuck r@ count + swap cmove r@ c@ + r@ c!
+        ELSE
+          dup [char] a [char] z 1+ within IF
+            [char] a - chars est-table + c@
+          THEN
+          r@ est-add-char
+        THEN
+      THEN
+      1 /string
+    THEN
+    rdrop
+  ELSE
+    drop
+  THEN
+;
+[THEN]
+
+
+[UNDEFINED] parse\" [IF]
+: parse\"   ( - c-addr u = Parse the input stream for a escaped string )
+  source >in @ /string tuck
+  pad >r
+  0 r@ c!                    \ Length destination = 0
+  BEGIN
+    dup IF
+      over c@ [char] " <>
+    ELSE
+      false
+    THEN
+  WHILE
+    over c@ dup [char] \ = IF
+      drop
+      1 /string r@ parse-esc
+    ELSE
+      r@ est-add-char
+      1 /string
+    THEN
+  REPEAT
+  dup IF
+    1 /string
+  THEN
+  r> count
+;
+[THEN]
+
+
+[UNDEFINED] s\" [IF]
+: s\"   ( "..." - c-addr u = Create a string with escaped characters )
+  parse\" state @ IF
+    postpone sliteral
+  ELSE
+    dup allocate throw swap 2>r 2r@ cmove 2r>
+  THEN
+; immediate
+[THEN]
+
+
+[UNDEFINED] ,\" [IF]
+: ,\"   ( "..." - = Store a string with escaped characters )
+  parse\"
+  here 
+  over char+ allot
+  place align
+;
+[THEN]
+
+[THEN]
+
+\ ==============================================================================
--- ffl/xos.fs
+++ ffl/xos.fs
@@ -20,7 +20,7 @@
 \
 \ ==============================================================================
 \ 
-\  $Date: 2007-11-24 18:43:21 $ $Revision: 1.2 $
+\  $Date: 2007-12-02 07:50:43 $ $Revision: 1.3 $
 \
 \ ==============================================================================
 
@@ -34,8 +34,11 @@ include ffl/tos.fs
 
 ( xos = XML/HTML writer )
 ( The xos module implements words for writing xml and html to an output      )
-( stream. The xos module extends the tos module, so the xos words words on   )
-( tos variables.                                                             )
+( stream. The xos module extends the tos module, so the xos words works on   )
+( tos variables. The module translates the normal entity references: &lt;,   )
+( &gt;, &quot; and &apos;. All other entity references should be written     )
+( with the xos-write-raw-text word. Note: balancing of start and end tags    )
+( are not checked, so the module can also be used to write html output.      )
 
 
 1 constant xos.version
@@ -43,7 +46,7 @@ include ffl/tos.fs
 
 ( Private words )
 
-: xos-write-string   ( c-addr u w:tos - = Write normal xml text )
+: xos-write-string   ( c-addr u w:tos - = Write normal xml text with entity reference translation )
   -rot
   bounds ?DO
     I c@ 
@@ -59,10 +62,9 @@ include ffl/tos.fs
   drop
 ;
 
-: xos-write-starting-tag  ( c-addr u c-addr u ... n c-addr u w:tos - = Write a xml starting tag with n attributes )
+: xos-write-name-attr  ( c-addr u c-addr u ... n c-addr u w:tos - = Write a xml name with n attributes )
   >r
-  [char] <  r@ tos-write-char
-            r@ tos-write-string 
+  r@ tos-write-string 
   BEGIN
     ?dup
   WHILE                           \ while nr attributes > 0
@@ -85,16 +87,18 @@ include ffl/tos.fs
   rdrop
 ;
 
+
 ( xml writer words )
 
-: xos-write-start-document   ( c-addr:standalone u c-addr u c-addr u w:tos - = Write the start of a xml document )
+: xos-write-start-xml   ( c-addr:standalone u c-addr:encoding u c-addr:version u w:tos - = Write the start of a xml document )
   >r
   s" <?xml version=" r@ tos-write-string
-  r@ tos-write-string
+  [char] " r@ tos-write-char
+           r@ tos-write-string
   [char] " r@ tos-write-char
   
   ?dup IF                         \ Optional encoding
-    s" encoding=" r@ tos-write-string
+    s"  encoding=" r@ tos-write-string
     [char] " r@ tos-write-char
     r@ tos-write-string
     [char] " r@ tos-write-char
@@ -103,7 +107,7 @@ include ffl/tos.fs
   THEN
   
   ?dup IF                         \ Optional standalone
-    s" standalone=" r@ tos-write-string
+    s"  standalone=" r@ tos-write-string
     [char] " r@ tos-write-char
     r@ tos-write-string
     [char] " r@ tos-write-char
@@ -114,14 +118,15 @@ include ffl/tos.fs
 ;
 
 
-: xos-write-text   ( c-addr u w:tos - = Write normal xml text )
+: xos-write-text   ( c-addr u w:tos - = Write normal xml text with translation to entity references )
   xos-write-string
 ;
 
 
 : xos-write-start-tag ( c-addr u c-addr u ... n c-addr u w:tos - = Write a xml start tag with n attributes )
   >r
-  r@ xos-write-starting-tag
+  [char] < r@ tos-write-char
+           r@ xos-write-name-attr
   [char] > r> tos-write-char
 ;
 
@@ -135,9 +140,10 @@ include ffl/tos.fs
 ;
 
 
-: xos-write-start-end-tag   ( c-addr u c-addr u ... n c-addr u w:tos - = Write a xml start and end tag with n attributes )
+: xos-write-empty-element   ( c-addr u c-addr u ... n c-addr u w:tos - = Write a xml start and end tag with n attributes )
   >r
-  r@ xos-write-starting-tag
+  [char] < r@ tos-write-char
+           r@ xos-write-name-attr
   [char] / r@ tos-write-char
   [char] > r> tos-write-char
 ;
@@ -150,21 +156,67 @@ include ffl/tos.fs
 
 : xos-write-comment ( c-addr u w:tos - = Write xml comment )
   >r
-  s" <--" r@ tos-write-string
+  s" <!--" r@ tos-write-string
           r@ tos-write-string
   s" -->" r> tos-write-string
 ;
 
 
-: xos-write-markup ( c-addr u ... n c-addr u w:tos - = Write a xml markup with n parameters )
+: xos-write-cdata   ( c-addr u w:tos - = Write a CDATA section )
+  >r
+  s" <![CDATA[" r@ tos-write-string
+                r@ tos-write-string
+  s" ]]>"       r> tos-write-string
 ;
 
 
-: xos-write-proc-instr ( c-addr u ... n c-addr u w:tos - = Write a xml processing instruction )
+: xos-write-proc-instr ( ...  c-addr:value u c-addr:name u n c-addr:target u w:tos - = Write a xml processing instruction )
+  >r
+  s" <?" r@ tos-write-string
+         r@ xos-write-name-attr
+  s" ?>" r> tos-write-string
 ;
 
 
-: xos-write-dtd   ( c-addr u c-addr u w:tos - = Write a document type definition )
+: xos-write-internal-dtd   ( c-addr:markup u c-addr:name u w:tos - = Write an internal document type definition )
+  >r
+  s" <!DOCTYPE " r@ tos-write-string
+                 r@ tos-write-string
+  s"  ["         r@ tos-write-string
+                 r@ tos-write-string   \ Write markup
+  s" ]>"         r> tos-write-string
+;
+
+
+: xos-write-system-dtd   ( c-addr:system u c-addr:name u w:tos - = Write a system document type definition )
+  >r
+  s" <!DOCTYPE " r@ tos-write-string
+                 r@ tos-write-string
+  s"  SYSTEM "   r@ tos-write-string
+  [char] "       r@ tos-write-char
+                 r@ tos-write-string   \ Write system
+  [char] "       r@ tos-write-char
+  [char] >       r> tos-write-char               
+;
+
+
+: xos-write-public-dtd   ( c-addr:system u c-addr:publicid u c-addr:name u w:tos - = Write a public document type definition )
+  >r
+  s" <!DOCTYPE " r@ tos-write-string
+                 r@ tos-write-string
+  s"  PUBLIC "   r@ tos-write-string
+  [char] "       r@ tos-write-char
+                 r@ tos-write-string   \ Write public id
+  [char] "       r@ tos-write-char
+  ?dup IF               
+    bl           r@ tos-write-char
+    [char] "     r@ tos-write-char
+                 r@ tos-write-string   \ Write system
+    [char] "     r@ tos-write-char
+  ELSE
+    drop
+  THEN
+  [char] >       r> tos-write-char               
 ;
 
 [THEN]
