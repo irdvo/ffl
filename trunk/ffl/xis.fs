ae2a84c2359e20026663f8f6059943e87d96ceb3
dick <dick@4faefe48-5067-11de-9cb9-2f00d435f02c>
2007-12-23 07:57:26 +0000

    xis tests

--- trunk/ffl/xis.fs
+++ trunk/ffl/xis.fs
@@ -20,13 +20,12 @@
 \
 \ ==============================================================================
 \ 
-\  $Date: 2007-12-16 19:37:18 $ $Revision: 1.8 $
+\  $Date: 2007-12-23 07:57:26 $ $Revision: 1.9 $
 \
 \ ==============================================================================
 
 
 include ffl/config.fs
-
 
 [UNDEFINED] xis.version [IF]
 
@@ -61,16 +60,16 @@ msc-create xis.entities  ( the default entity reference catalog )
   s" apos" s" '"   xis.entities msc-add
 
 
-chs-create xis.start-name-set   ( the character set for the first letter of a name )
+chs-create xis.start-name   ( the character set for the first letter of a name )
 
-         xis.start-name-set chs-set-alpha
-  s" _:" xis.start-name-set chs-set-string
+         xis.start-name chs-set-alpha
+  s" _:" xis.start-name chs-set-string  \ ToDo: during compilation -> string gone
 
 
-chs-create xis.next-name-set   ( the character set for the following letters of a name )
+chs-create xis.next-name   ( the character set for the following letters of a name )
 
-           xis.next-name-set chs-set-alnum
-  s" .-_:" xis.next-name-set chs-set-string
+           xis.next-name chs-set-alnum
+  s" .-_:" xis.next-name chs-set-string
 
 
 chs-create xis.space   ( the character set for xml space )
@@ -80,7 +79,32 @@ chs-create xis.space   ( the character set for xml space )
   chr.cr xis.space chs-set-char
   chr.lf xis.space chs-set-char
 
+  
+chs-create xis.end-normal-text   ( the ending character set for normal text )
+  
+  char < xis.end-normal-text chs-set-char
+  char & xis.end-normal-text chs-set-char
+  
 
+chs-create xis.end-quoted-value   ( the ending character set for a quoted attribute value )
+  
+  char " xis.end-quoted-value chs-set-char
+  char & xis.end-quoted-value chs-set-char
+  
+  
+chs-create xis.end-apos-value   ( the ending character set for a single quoted attribute value )
+  
+  char ' xis.end-apos-value chs-set-char
+  char & xis.end-apos-value chs-set-char
+  
+  
+chs-create xis.end-non-quoted-value   ( the ending character set for a non-quoted attribute value )
+
+  xis.space xis.end-non-quoted-value chs^move
+  char &    xis.end-non-quoted-value chs-set-char
+  char >    xis.end-non-quoted-value chs-set-char
+  
+  
 ( xis reader constants )
 
 begin-enumeration
@@ -177,18 +201,22 @@ end-structure
 
 : xis-read-reference   ( xis -- = Read and translate the reference )
   >r
-  [char] ; r@ tis-scan-char IF              \ Look for the end of the reference
-    dup 1+ 1+                               \ Calculate length of reference
-    -rot                                    \ And save it
-    r@ xis-msc@ msc-translate? IF           \ Try to translate the reference
-      tuck                                  \ Save translated length
-      2over drop r@ tis-pntr@ swap -        \ Calculate the insert index
-      r@ str-insert-string                  \ Insert the translated reference in the stream
-      2dup over r@ tis-pntr@ swap - +       \ Calculate the delete index
-      r@ str-delete                         \ Delete the old reference
-      swap - r@ tis-pntr+! drop             \ Update the stream pointer
-    ELSE
-      drop
+  [char] # r@ tis-cmatch-char IF
+    \ ToDo: numerical reference
+  ELSE
+    [char] ; r@ tis-scan-char IF              \ Look for the end of the reference
+      dup 1+ 1+                               \ Calculate length of reference
+      -rot                                    \ And save it
+      r@ xis-msc@ msc-translate? IF           \ Try to translate the reference
+        tuck                                  \ Save translated length
+        2over drop r@ tis-pntr@ swap -        \ Calculate the insert index
+        r@ str-insert-string                  \ Insert the translated reference in the stream
+        2dup over r@ tis-pntr@ swap - +       \ Calculate the delete index
+        r@ str-delete                         \ Delete the old reference
+        swap - r@ tis-pntr+! drop             \ Update the stream pointer
+      ELSE
+        drop
+      THEN
     THEN
   THEN
   rdrop
@@ -214,20 +242,13 @@ end-structure
 ;
 
 
-: xis-read-attributes ( c-addr1 u1 xis -- c-addrn un c-addrn un .. n c-addr1 u1 true | false = Read tag attributes )
-  drop true
-  \ Lus: read attribute
-  \ Error: remove from stack
-;
-
-
 : xis-read-name   ( xis -- c-addr u | 0 = Read a xml name )
   >r
   r@ tis-pntr@
-  xis.start-name-set r@ tis-match-set IF
+  xis.start-name r@ tis-match-set IF
     drop
     BEGIN
-      xis.next-name-set r@ tis-match-set
+      xis.next-name r@ tis-match-set
     WHILE
       drop
     REPEAT
@@ -239,27 +260,95 @@ end-structure
 ;
 
 
+: xis-read-ref-text   ( chs xis -- c-addr u = Read text till set with translation of references )
+  >r
+  r@ tis-pntr@ swap
+  BEGIN
+    dup r@ tis-scan-set IF                  \ Scan text till set
+      nip nip
+      [char] & = IF
+        true
+      ELSE 
+        -1 r@ tis-pntr+! drop               \ Set trailing char unprocessed
+        false
+      THEN
+    ELSE
+      r@ tis-read-all IF                    \ Nothing from set found, all remaining text is normal text
+        drop
+      THEN
+      false
+    THEN
+  WHILE
+    r@ xis-read-reference                   \ Repeat for entity reference
+  REPEAT
+  drop
+  
+  r> tis-substring                          \ Get the full text string
+;
+
+
+: xis-read-value   ( xis -- c-addr u = Read attribute value )
+  >r
+  [char] " r@ tis-cmatch-char IF
+    xis.end-quoted-value r@ xis-read-ref-text
+    [char] " r@ tis-cmatch-char drop       \ Remove trailing double quote
+  ELSE
+    [char] ' r@ tis-cmatch-char IF
+      xis.end-apos-value r@ xis-read-ref-text
+      [char] ' r@ tis-cmatch-char drop     \ Remove trailing single quote
+    ELSE
+      xis.end-non-quoted-value r@ xis-read-ref-text
+    THEN
+  THEN
+  rdrop
+;
+
+
+: xis-read-attributes ( c-addr1 u1 xis -- c-addrn un c-addrn un .. n c-addr1 u1 = Read tag attributes )
+  0
+  >r >r
+
+  BEGIN
+    r@ xis-skip-spaces
+    r@ xis-read-name ?dup
+    
+    IF
+      r@ xis-skip-spaces
+      [char] = r@ tis-cmatch-char IF
+        r@ xis-skip-spaces
+        r@ xis-read-value
+      ELSE
+        nil 0                          \ Attribute without value
+      THEN
+      2swap 2rot                       \ Swap the value and name and rotate it after tag
+      r> r> 1+ >r >r                   \ Increment the attribute counter
+      false
+    ELSE
+      true
+    THEN
+  UNTIL
+  rdrop
+  r> -rot
+;
+
+
 : xis-read-start-tag ( xis -- c-addrn un c-addrn un .. n c-addr u xis.start-tag = Read a start tag )
   >r
   r@ xis-read-name ?dup IF
-    r@ xis-read-attributes IF
-      r@ xis-skip-spaces
-      s" />" r@ tis-cmatch-string IF
-        xis.empty-element
-      ELSE
-        [char] > r@ tis-cmatch-char IF
-          xis.start-tag
-        ELSE
-          xis+error-tag
-          xis.error
-        THEN
-      THEN
+    r@ xis-read-attributes
+    r@ xis-skip-spaces
+    s" />" r@ tis-cmatch-string IF
+      xis.empty-element
     ELSE
-      2r> 2drop
-      xis.error
+      [char] > r@ tis-cmatch-char IF
+        xis.start-tag
+      ELSE
+~~        xis+error-tag
+        xis.error
+      THEN
     THEN
   ELSE
-    drop xis.error
+    xis.error
   THEN
   rdrop
 ;
@@ -299,20 +388,16 @@ end-structure
 : xis-read-proc-instr ( xis -- c-addrn un c-addrn un .. n c-addr u = Read a processing instruction )
   >r
   r@ xis-read-name ?dup IF
-    xis-read-attributes IF
-      r@ xis-skip-spaces
-      s" ?>" r@ tis-cmatch-string IF
-        2dup s" xml" icompare 0= IF
-          xis.start-xml
-        ELSE
-          xis.proc-instr
-        THEN
+    xis-read-attributes
+    r@ xis-skip-spaces
+    s" ?>" r@ tis-cmatch-string IF
+      2dup s" xml" icompare 0= IF
+        xis.start-xml
       ELSE
-        xis+error-tag
-        xis.error
+        xis.proc-instr
       THEN
     ELSE
-      2r> 2drop
+      xis+error-tag
       xis.error
     THEN
   ELSE
@@ -337,36 +422,19 @@ end-structure
 
 
 : xis-read-entity   ( xis -- i*x n = Read the entity definition )
+  \ ToDo
 ;
 
 
 : xis-read-doctype   ( xis -- i*x n = Read the document type definition )
+  \ ToDo
 ;
 
 
 : xis-read-text   ( xis -- c-addr u n | n = Read normal xml text, return xis.text or xis.done )
   >r
-  r@ tis-pntr@
-  BEGIN
-    s" <&" r@ tis-scan-chars IF             \ Scan text till < or &
-      nip nip
-      [char] & = IF
-        true
-      ELSE
-        -1 r@ tis-pntr+! drop               \ Set < unprocessed
-        false
-      THEN
-    ELSE
-      r@ tis-read-all IF                    \ No < or & found, all remaining text is normal text
-        drop
-      THEN
-      false
-    THEN
-  WHILE
-    r@ xis-read-reference                   \ Repeat for entity reference
-  REPEAT
   
-  r@ tis-substring                          \ Get the full text string
+  xis.end-normal-text r@ xis-read-ref-text  \ Read till <, translate & references
   
   ?dup IF                                   \ If text read Then
     r@ xis-strip@ IF                        \   Strip it if requested
@@ -447,6 +515,7 @@ end-structure
 ( Inspection )
 
 : xis-dump ( xis -- = Dump the xml reader variable )
+  \ ToDo
 ;
 
 [THEN]
--- trunk/test/xis_test.fs
+++ trunk/test/xis_test.fs
@@ -20,7 +20,7 @@
 \
 \ ==============================================================================
 \ 
-\  $Date: 2007-12-16 19:37:18 $ $Revision: 1.4 $
+\  $Date: 2007-12-23 07:57:26 $ $Revision: 1.5 $
 \
 \ ==============================================================================
 
@@ -33,6 +33,104 @@ include ffl/tst.fs
 
 t{ xis-create xis1 }t
 
+\ Single Tag tests
+
+t{ s" <tag>" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag ?s s" tag" compare ?0 ?0 }t
+
+t{ s" hello<tag>bye" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.text          ?s s" hello" compare ?0 }t
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 ?0 }t
+t{ xis1 xis-read xis.text          ?s s" bye" compare ?0 }t
+
+\ Tag with attribute without value test
+
+t{ s" <tag attr1>" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 ?0 ?nil }t
+
+t{ s" hello<tag attr1>bye" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.text          ?s s" hello" compare ?0 }t
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 ?0 ?nil }t
+t{ xis1 xis-read xis.text          ?s s" bye" compare ?0 }t
+
+
+\ Tag with attribute with non-quoted value
+
+t{ s" <tag attr1=value1>" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value1" compare ?0 }t
+
+t{ s" <tag attr1 = value1>" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value1" compare ?0 }t
+
+t{ s" <tag attr1=value1 >" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value1" compare ?0 }t
+
+t{ s" <tag attr1=&lt;1&amp;2&gt;>" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag .s    ?s 2dup type cr s" tag" compare ?0 1 ?s 2dup type cr s" attr1" compare ?0 2dup type cr s" <1&2>" compare ?0 }t
+
+t{ s" hello<tag attr1=value1>bye" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.text          ?s s" hello" compare ?0 }t
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value1" compare ?0 }t
+t{ xis1 xis-read xis.text          ?s s" bye" compare ?0 }t
+
+
+\ Tag with attribute with single-quoted value
+
+t{ s" <tag attr1='value 1'>" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value 1" compare ?0 }t
+
+t{ s" <tag attr1 = 'value 1'>" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value 1" compare ?0 }t
+
+t{ s" <tag attr1='value 1' >" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value 1" compare ?0 }t
+
+t{ s" hello<tag attr1='value 1'>bye" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.text          ?s s" hello" compare ?0 }t
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value 1" compare ?0 }t
+t{ xis1 xis-read xis.text          ?s s" bye" compare ?0 }t
+
+
+\ Tag with attribute with double-quoted value
+
+t{ s\" <tag attr1=\"value 1\">" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value 1" compare ?0 }t
+
+t{ s\" <tag attr1 = \"value 1\">" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value 1" compare ?0 }t
+
+t{ s\" <tag attr1=\"value 1\" >" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value 1" compare ?0 }t
+
+t{ s\" hello<tag attr1=\"value 1\">bye" xis1 xis-set-string }t
+
+t{ xis1 xis-read xis.text          ?s s" hello" compare ?0 }t
+t{ xis1 xis-read xis.start-tag     ?s s" tag" compare ?0 1 ?s s" attr1" compare ?0 s" value 1" compare ?0 }t
+t{ xis1 xis-read xis.text          ?s s" bye" compare ?0 }t
+
+
+0 [IF]
+
+[THEN]
+
+\ Other tests
+
 t{ s" <!--Comment1--><![CDATA[Hallo]]>&gt;Test1&amp;Test2&unknown;Test3&lt;<!--Comment2--></tag ><bold />" xis1 xis-set-string }t 
 
 t{ xis1 xis-read xis.comment       ?s s" Comment1" compare ?0 }t
@@ -40,5 +138,6 @@ t{ xis1 xis-read xis.cdata         ?s s" Hallo"    compare ?0 }t
 t{ xis1 xis-read xis.text          ?s s" >Test1&Test2&unknown;Test3<" compare ?0 }t
 t{ xis1 xis-read xis.comment       ?s s" Comment2" compare ?0 }t
 t{ xis1 xis-read xis.end-tag       ?s s" tag"      compare ?0 }t
-t{ xis1 xis-read xis.empty-element ?s s" bold"     compare ?0 }t
+\ t{ xis1 xis-read xis.empty-element ?s s" bold"     compare ?0 }t
+
 \ ==============================================================================
