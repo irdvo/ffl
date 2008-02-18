270624bf166b8a98950b193667d20b42eb60dd4c
dick <dick@4faefe48-5067-11de-9cb9-2f00d435f02c>
2008-02-18 06:35:28 +0000

    dom tests

--- trunk/test/dom_test.fs
+++ trunk/test/dom_test.fs
@@ -20,7 +20,7 @@
 \
 \ ==============================================================================
 \ 
-\  $Date: 2008-02-17 07:37:16 $ $Revision: 1.2 $
+\  $Date: 2008-02-18 06:35:28 $ $Revision: 1.3 $
 \
 \ ==============================================================================
 
@@ -45,13 +45,108 @@ t{ s" test.xml" r/o open-file throw value dom.file }t
 
 t{ dom.file ' dom-test-reader dom1 dom-read-reader ?true }t
 
-\ Iterate the tree
+\ Iterate and modify the tree
 
-\ Modify the tree
+t{ dom1 dom-document ?true }t
+
+t{ dom1 dom-document? ?true }t
+
+t{ dom1 dom-parent ?false }t
+
+t{ dom1 dom-document ?true }t
+
+t{ dom1 dom-children 7 ?s }t
+
+t{ dom1 dom-children? ?true }t
+
+t{ dom1 dom-child  ?true dom.attribute ?s }t
+
+t{ dom1 dom-first?    ?true  }t
+t{ dom1 dom-last?     ?false }t
+t{ dom1 dom-children? ?false }t
+
+t{ dom1 dom-get-name  s" standalone" ?str }t
+t{ dom1 dom-get-value s" no"     ?str }t
+
+
+t{ dom1 dom-next ?true dom.attribute ?s }t
+
+t{ dom1 dom-first?    ?false }t
+t{ dom1 dom-last?     ?false }t
+t{ dom1 dom-children? ?false }t
+
+t{ dom1 dom-get-name  s" version" ?str }t
+t{ dom1 dom-get-value s" 1.0"     ?str }t
+
+t{ s" version" s" 1.1" dom1 dom-set }t
+
+t{ dom1 dom-next ?true dom.text ?s }t
+
+t{ dom1 dom-next ?true dom.comment ?s }t
+
+t{ dom1 dom-first?    ?false }t
+t{ dom1 dom-last?     ?false }t
+t{ dom1 dom-children? ?false }t
+
+t{ dom1 dom-get-value s"  This is a test file for the ffl-library " ?str }t
+
+t{ s"  This is a (modified) test file " dom1 dom-set }t
+
+t{ dom1 dom-next ?true dom.text ?s }t
+
+t{ dom1 dom-next ?true dom.element ?s }t
+
+t{ dom1 dom-first?    ?false }t
+t{ dom1 dom-last?     ?false }t
+t{ dom1 dom-children? ?true  }t
+
+t{ dom1 dom-next ?true dom.text ?s }t
+
+t{ dom1 dom-first?    ?false }t
+t{ dom1 dom-last?     ?true  }t
+t{ dom1 dom-children? ?false }t
+
+t{ dom1 dom-prev ?true dom.element ?s }t
+
+t{ dom1 dom-get-name s" TEST" ?str }t
+
+t{ dom1 dom-child ?true dom.text ?s }t
+
+t{ dom1 dom-next ?true dom.element ?s }t
+
+t{ dom1 dom-get-name s" BOOK" ?str }t
+
+t{ s" BOOKLET" dom1 dom-set }t
+
+t{ s" SUBJECT" dom.element dom1 dom-append-node }t
+
+t{ s" MAIN" s" Computers"   dom.attribute dom1 dom-append-node }t
+t{ s" SUB"  s" Programming" dom.attribute dom1 dom-insert-node-before }t
+t{ s" Introduction to the " dom.text dom1 dom-insert-node-after }t
+t{ dom1 dom-next ?true dom.text ?s }t
+t{ s" forth programming language" dom.text dom1 dom-insert-node-after }t
+
+t{ dom1 dom-parent ?true dom.element ?s }t
+t{ dom1 dom-parent ?true dom.element ?s }t  \ back to booklet
+
+t{ dom1 dom-child ?true dom.text ?s }t
+t{ dom1 dom-next  ?true dom.element ?s }t
+t{ dom1 dom-next  ?true dom.text ?s }t
+t{ dom1 dom-next  ?true dom.element ?s }t
+
+t{ dom1 dom-child ?true dom.text ?s }t
+t{ dom1 dom-next  ?true dom.element ?s }t
+t{ dom1 dom-next  ?true dom.text ?s }t
+t{ dom1 dom-next  ?true dom.element ?s }t
+
+t{ dom1 dom-get-name s" FIRSTBOOK" ?str }t
+
+t{ dom1 dom-remove ?true }t  \ remove first book
+t{ dom1 dom-last?  ?true }t
+t{ dom1 dom-remove ?true }t  \ remove trailing spaces
 
 \ Write the tree
 
 t{ dom1 dom-write-string ?true type }t
- 
 
 \ ==============================================================================
