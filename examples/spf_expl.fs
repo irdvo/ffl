11dca21dec5b0858491b102e9a3cffffa499335d
dvoudheusden <dvoudheusden@4faefe48-5067-11de-9cb9-2f00d435f02c>
2009-08-16 06:24:39 +0000

    %e and %E added to sprintf module

--- examples/spf_expl.fs
+++ examples/spf_expl.fs
@@ -26,30 +26,34 @@
 
 include ffl/spf.fs
 
-str-new value spf1                             \ Create string as destination
+str-new value spf1                                     \ Create string as destination
 
 .( Format numbers                       : ) 
-15 15 10 8 1 s" %d %u %o %x %X" spf1 spf-set   \ Format the numbers
+[DEFINED] represent [IF]
+18E+0 15 15 10 8 1 s" %d %u %o %x %X %E" spf1 spf-set  \ Format the numbers
+[ELSE]
+18E+0 15 15 10 8 1 s" %d %u %o %x %X %E" spf1 spf-set  \ Format the numbers
+[THEN]
 
-spf1 str-get type cr                           \ Print the result
+spf1 str-get type cr                                   \ Print the result
 
 .( Format strings and characters        : )
-char ! s" Hello" s" %s %c" spf1 spf-set        \ Format strings and characters
+char ! s" Hello" s" %s %c" spf1 spf-set                \ Format strings and characters
 
-spf1 str-get type cr                           \ Print result
+spf1 str-get type cr                                   \ Print result
 
 .( Format numbers with signs            : )
--30 20 10 s" %+d % d %d" spf1 spf-set          \ Format numbers with signs
+-30 20 10 s" %+d % d %d" spf1 spf-set                  \ Format numbers with signs
 
-spf1 str-get type cr                           \ Print result
+spf1 str-get type cr                                   \ Print result
 
 .( Leading and trailing zeros and spaces: )
--40 -30 20 10 s" %5d %-+5d %05d %5d" spf1 spf-set  \ Format numbers with leading and trailing spaces
+-40 -30 20 10 s" %5d %-+5d %05d %5d" spf1 spf-set      \ Format numbers with leading and trailing spaces
 
 spf1 str-get type cr
 
 .( Format with parsing word             : )
-time&date spf1 spf" %04d-%02d-%2d %02d:%02d:%02d"  \ Format date & time
+time&date spf1 spf" %04d-%02d-%2d %02d:%02d:%02d"      \ Format date & time
 
-spf1 str-get type cr                           \ Print result
+spf1 str-get type cr                                   \ Print result
 
