07f5677537663e1d6a3ee839c603e24e2d809171
ruv <ruvim@forth.org.ru>
2016-09-12 04:01:09 +0300

    fix engines/spf/config -- '1-!' is missed
    The word '1-!' was used before it was defined
    (it is defined in tlb.fs).
    The issue arose in the last refactoring,
    commit aa83dd64 "Toolbelt cont" on 2016-07-19

--- engines/spf/config.fs
+++ engines/spf/config.fs
@@ -189,6 +189,10 @@ TRUE CONSTANT FLOATING-EXT
 
 [THEN]
 
+( Toolbelt )
+
+include ffl/tlb.fs
+
 
 ( Exceptions )
 
@@ -210,10 +214,6 @@ s" Wrong file data"    exception constant exp-wrong-file-data    ( -- n = Wrong 
 s" Wrong checksum"     exception constant exp-wrong-checksum     ( -- n = Wrong checksum )
 s" Wrong length"       exception constant exp-wrong-length       ( -- n = Wrong length )
 s" Invalid data"       exception constant exp-invalid-data       ( -- n = Invalid data exception number )
-
-( Toolbelt )
-
-include ffl/tlb.fs
 
 [ELSE]
   drop
